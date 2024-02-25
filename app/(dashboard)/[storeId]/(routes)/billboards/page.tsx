16a354e44fc1e99fb40fd06ef0db60d7e8d2d051
Eduardq <104719717+Eduardq@users.noreply.github.com>
2024-02-25 21:21:29 +0200

    first commit

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -0,0 +1,30 @@
+import { format } from "date-fns";
+import { BillboardClient } from "./components/client";
+import prismadb from "@/lib/prismadb";
+import { BillboardColumn } from "./components/columns";
+
+const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createedAt: "desc",
+    },
+  });
+
+  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    id: item.id,
+    label: item.label,
+    createedAt: format(item.createedAt, "MMMM do, yyyy"),
+  }));
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardClient data={formattedBillboards} />
+      </div>
+    </div>
+  );
+};
+
+export default BillboardsPage;
